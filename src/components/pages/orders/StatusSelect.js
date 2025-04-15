1bed27f1dbb19629a6d12a768df866120879d345
xk-shams <olimjonshamsiddinov04@gmail.com>
2025-04-15 18:01:36 +0500

    dsaw

--- src/components/pages/orders/StatusSelect.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function StatusSelect() {
-  return (
-    <div>StatusSelect</div>
-  )
-}
-
-export default StatusSelect
--- src/pages/_app.js
+++
@@ -1,5 +0,0 @@
-import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return Component.getLayout ? Component.getLayout(pageProps) : (<Component {...pageProps} />);
-}
